7607eb2974af29c96d8e0c4c2e87c5c4c38f8525
FLFfang <flffang28@icloud.com>
2025-03-12 22:19:22 +0800

    修改JdkSerializer解决java.io.EOFException localhost:8080报错

--- ff-rpc-basic/src/main/java/com/ff/ffrpc/serializer/JdkSerializer.java
+++ ff-rpc-basic/src/main/java/com/ff/ffrpc/serializer/JdkSerializer.java
@@ -14,7 +14,7 @@ import java.io.*;
 public class JdkSerializer implements Serializer {
 
     /**
-     * 序列化
+     * 序列化 - 基于 JDK 动态代理
      *
      * @param object
      * @param <T>
@@ -23,15 +23,24 @@ public class JdkSerializer implements Serializer {
      */
     @Override
     public <T> byte[] serialize(T object) throws IOException {
+        //创建一个字节数组输出流，用于存储序列化之后的数据
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        objectOutputStream.writeObject(object);
-        objectOutputStream.close();
-        return outputStream.toByteArray();
+
+        try {
+            // 将对象写入输出流 (序列化)
+            objectOutputStream.writeObject(object);
+            objectOutputStream.flush();
+            // 返回序列化后的字节数组
+            return outputStream.toByteArray();
+        } finally {
+            // 关闭输出流，释放资源
+            objectOutputStream.close();
+        }
     }
 
     /**
-     * 反序列化
+     * 反序列化 - 基于 JDK
      *
      * @param bytes
      * @param type
@@ -40,7 +49,12 @@ public class JdkSerializer implements Serializer {
      * @throws IOException
      */
     @Override
+    @SuppressWarnings("all")
     public <T> T deserialize(byte[] bytes, Class<T> type) throws IOException {
+        // 检查字节数组是否为空或长度为 0
+        if (bytes == null || bytes.length == 0) {
+            throw new IOException("字节数组为空，无法反序列化");
+        }
         ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
         ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
         try {
